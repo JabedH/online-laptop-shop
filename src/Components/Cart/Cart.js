2056eed09c022ded2b61e29fb5ef952effc5833f
JabedH <87475551+JabedH@users.noreply.github.com>
2022-03-25 23:14:38 +0600

    add

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
+  const { cart } = props;
+  console.log(props);
   return (
     <div className="cart">
       <h1>Selected Laptops</h1>
       <div className="text">
+        {cart.map((item) => (
+          <div>
+            <img src="" alt="" />
+            <h5>{item.name}</h5>
+          </div>
+        ))}
         <p>Selected Items: </p>
         <div className="my-btn">
           <button>CHOOSE 1 FOR ME</button>
